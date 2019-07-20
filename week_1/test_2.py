45a7d0e877b8407df8399717d86f18c51c8fad1c
Anton <test2783600@gmail.com>
2019-07-21 02:14:45 +0300

    week_1 version_0.1

---
+++ week_1/test_2.py
@@ -0,0 +1,11 @@
+num = int(input())
+penguin_1 = '   _~_    '
+penguin_2 = '  (o o)   '
+penguin_3 = ' /  V  \\  '
+penguin_4 = '/(  _  )\\ '
+penguin_5 = '  ^^ ^^   '
+print(num * penguin_1)
+print(num * penguin_2)
+print(num * penguin_3)
+print(num * penguin_4)
+print(num * penguin_5)
